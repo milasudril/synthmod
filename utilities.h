cace61fdfdbb7bf6a974bf83c258cedba1eb9cad
torbjorr <milasudril@gmail.com>
2014-08-10 12:01:35 +0200

    utilites.h: Use approximative exp2 to get some performance gain

--- utilities.h
+++ utilities.h
@@ -5,20 +5,21 @@ target[name[utilities.h] type[include]]
 #ifndef SYNTHMOD_UTILITIES_H
 #define SYNTHMOD_UTILITIES_H
 
+#include <mathext/functions.h>
 #include <cmath>
 
 namespace Synthmod
 	{
 	inline float keyToFrequency(int key,float f_s)
 		{
-		return 440.0f*pow(2.0f,(key-69)/12.0f)/f_s;
+		return 440.0f*MathExt::exp2_fast((key-69)/12.0f)/f_s;
 		}
 
 	inline float keyToFrequency(float key,float f_s)
 		{
-		return 440.0f*pow(2.0f,(key-69.0f)/12.0f)/f_s;
+		return 440.0f*MathExt::exp2_fast((key-69.0f)/12.0f)/f_s;
 		}
-	
+
 	inline float frequencyToKey(float f)
 		{
 		return 12.0f*log(f/440.0f)/log(2.0f) + 69.0f;
